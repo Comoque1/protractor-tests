464136348f184953fd3f8d13bac78e43f1347456
Comoque1 <comoque@gmail.com>
2020-06-10 23:20:36 +0300

    Updated Checkbox and Sort tests

--- tests/grid-page-object.js
+++ tests/grid-page-object.js
@@ -3,16 +3,37 @@ var GridPageObjects = function () {
     // Search objects
 
     // Sets value in search field 
-    var search = element(by.css('input.k-textbox'));
+    var searchInput = element(by.css('input.k-textbox'));
     this.searchInAllColumns = async function (name) {
-        await search.sendKeys(name);
+        await searchInput.sendKeys(name);
     }
 
-    // Checkbox objects - ToDo
-    // var gridCheckbox = element.all(by.css('td .k-checkbox')).get();
-    // this.setGridCheckbox = async function () {
-    //     await gridCheckbox.click();
-    // }
+    // Clears search input field
+    this.clearSreachField = async function(){
+        await searchInput.clear();
+    }
+
+    // Checkbox objects 
+    // Note 1: the parameter "val" is the index of the checkbox starting from 0
+    // Note 2: index 0 is "select all" checkbox 
+    var gridCheckbox = element.all(by.css('kendo-grid .k-checkbox'));
+    this.setGridCheckbox = async function (val) {
+        await gridCheckbox.get(val).click();
+    }
+
+    // Get the state of the checkbox 
+    // Note 1: use the same "val" as in the "setGridCheckbox" function
+    // Note 2: index 0 is "select all" checkbox
+    this.checkStateGridCheckbox = function (val) {
+        return gridCheckbox.get(val);
+    }
+
+    // Sorts grid column. 
+    // Note 1: the parameter "val" is the number of the column starting from 0
+    var sortColumn = element.all(by.css('th span.k-link'));
+    this.pressSortColumn = async function (val) {
+        await sortColumn.get(val).click();
+    }
 
     // Gets number of rows from grid
     var rowsNumber = element.all(by.css('kendo-grid-list tr'));
@@ -24,6 +45,12 @@ var GridPageObjects = function () {
     var contactDetails = element.all(by.css('kendo-grid td'));
     this.getContactDetails = function () {
         return contactDetails.getText();
+    }
+
+    // Gets text from all rows for a columns
+    let jobTitle = element.all(by.css('td[data-kendo-grid-column-index=\'2\']'));
+    this.getjobTitle = async function () {
+        return await jobTitle.getText();
     }
 
     // Gets text: No records available.
--- tests/spec.js
+++ tests/spec.js
@@ -5,55 +5,55 @@ var gridPageObjects = require('./grid-page-object.js');
 
 beforeAll(async function () {
     await browser.waitForAngularEnabled(true);
-})
+});
 beforeEach(async function () {
     await browser.get('http://localhost:4200/');
-})
+});
 
 describe('Search', function () {
     it('should return data in grid area', async function () {
         for (var i = 0; i <= 3; i++) {
             await gridPageObjects.searchInAllColumns(searchJsonData[i]);
             await expect(gridPageObjects.getContactDetails()).toContain(searchJsonData[i]);
-            await element(by.css('input.k-textbox')).clear();
-        }
-    })
+            await gridPageObjects.clearSreachField();
+        };
+    });
 
     it('should Not return data in grid area', async function () {
         for (var i = 0; i <= 1; i++) {
             await gridPageObjects.searchInAllColumns(searchJsonDataNoRecords[i]);
             await expect(gridPageObjects.getContactDetails()).toContain('No records available.');
             await expect(gridPageObjects.getGridItems()).toEqual(['0 - 0 of 0 items']);
-            expect(gridPageObjects.getContactDetails().count()).toEqual(1);
-            await element(by.css('input.k-textbox')).clear();
-        }
-    })
-}),
+            await expect(gridPageObjects.getContactDetails().count()).toEqual(1);
+            await gridPageObjects.clearSreachField();
+        };
+    });
+});
 
-    describe('Checkbox', function () {
-        it('should set checkbox to first row', async function () {
-            var gridCheckbox = await element.all(by.css('kendo-grid-list input')).get(0);
-            gridCheckbox.click();
-            expect(gridCheckbox.isSelected()).toBe(true);
-        });
+describe('Checkbox', function () {
+    it('should set checkbox to first row', async function () {
+        await gridPageObjects.setGridCheckbox(1);
+        await expect(gridPageObjects.checkStateGridCheckbox(1).isSelected()).toBe(true);
+        await expect(gridPageObjects.checkStateGridCheckbox(2).isSelected()).toBe(false);
+    });
 
-        it('should set checkbox to all rows', async function () {
-            var gridCheckbox = await element(by.css('input#k-grid0-select-all'));
-            gridCheckbox.click();
-            expect(gridCheckbox.isSelected()).toBe(true);
+    it('should set checkbox to all rows', async function () {
+        await gridPageObjects.setGridCheckbox(0);
+        for (var i = 0; i <= 20; i++) {
+            await expect(gridPageObjects.checkStateGridCheckbox(i).isSelected()).toBe(true);
+        };
+    });
+});
 
-        });
-    }),
-
-    describe('Sort', function () {
-        it('should sort by job title', async function () {
-
-            await gridPageObjects.searchInAllColumns('meg');
-            expect(gridPageObjects.getRowsNumber()).toEqual(2);
-            var b = await element.all(by.css('th span.k-link')).get(1);
-            b.click();
-        })
-    })
+describe('Sort', function () {
+    it('should sort by job title', async function () {
+        await gridPageObjects.searchInAllColumns('meg');
+        await expect(gridPageObjects.getRowsNumber()).toEqual(2);
+        await expect(gridPageObjects.getjobTitle(1)).toEqual(['Operator', 'Librarian']);
+        await gridPageObjects.pressSortColumn(1);
+        await expect(gridPageObjects.getjobTitle(1)).toEqual(['Librarian', 'Operator']);
+    });
+});
 
 
 
